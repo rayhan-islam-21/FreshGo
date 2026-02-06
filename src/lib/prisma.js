9cb3f7c562238cce029afb3c3bb1d85625db4b5b
Rayhan Islam <merayhanislam21@gmail.com>
2026-02-06 16:50:52 +0600

    done prisma setup fully with postgreSql

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,11 +1,10 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  // Prevent multiple instances in dev (Next.js hot reload)
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
