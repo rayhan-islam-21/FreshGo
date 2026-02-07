597492b26b3e6af26e7789d8b4ecd1721a9883a5
Rayhan Islam <merayhanislam21@gmail.com>
2026-02-07 15:12:35 +0600

    sidebar done

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -22,36 +22,36 @@ export const sidebarLinks = [
   {
     category: "Main menu",
     items: [
-      { name: "Dashboard", slug: "/dashboard", icon: <LuLayoutDashboard /> },
-      { name: "Order Management", slug: "/orders", icon: <LuShoppingCart /> },
-      { name: "Customers", slug: "/customers", icon: <LuUsers /> },
-      { name: "Coupon Code", slug: "/coupons", icon: <LuTicket /> },
-      { name: "Categories", slug: "/categories", icon: <LuLayers /> },
-      { name: "Transaction", slug: "/transactions", icon: <LuWallet /> },
-      { name: "Brand", slug: "/brands", icon: <LuStar /> },
+      { name: "Dashboard", slug: "/dashboard", icon: <LuLayoutDashboard size={20} /> },
+      { name: "Order Management", slug: "/orders", icon: <LuShoppingCart size={20} /> },
+      { name: "Customers", slug: "/customers", icon: <LuUsers size={20} /> },
+      { name: "Coupon Code", slug: "/coupons", icon: <LuTicket size={20} /> },
+      { name: "Categories", slug: "/categories", icon: <LuLayers size={20} /> },
+      { name: "Transaction", slug: "/transactions", icon: <LuWallet size={20} /> },
+      { name: "Brand", slug: "/brands", icon: <LuStar size={20} /> },
     ],
   },
   {
     category: "Product",
     items: [
-      { name: "Add Products", slug: "/products/add", icon: <LuPlus /> },
-      { name: "Product Media", slug: "/products/media", icon: <LuImage /> },
-      { name: "Product List", slug: "/products/list", icon: <LuPackage /> },
+      { name: "Add Products", slug: "/products/add", icon: <LuPlus size={20} /> },
+      { name: "Product Media", slug: "/products/media", icon: <LuImage size={20} /> },
+      { name: "Product List", slug: "/products/list", icon: <LuPackage size={20} /> },
       {
         name: "Product Reviews",
         slug: "/products/reviews",
-        icon: <LuMessageSquare />,
+        icon: <LuMessageSquare size={20} />,
       },
     ],
   },
   {
     category: "Admin",
     items: [
-      { name: "Admin role", slug: "/admin/roles", icon: <LuUserCheck /> },
+      { name: "Admin role", slug: "/admin/roles", icon: <LuUserCheck size={20} /> },
       {
         name: "Control Authority",
         slug: "/admin/control",
-        icon: <LuShieldCheck />,
+        icon: <LuShieldCheck size={20} />,
       },
     ],
   },
@@ -59,15 +59,15 @@ export const sidebarLinks = [
 
 const Sidebar = () => {
   return (
-    <div className="w-full p-3">
+    <div className="w-full p-3 ">
       {/* logo and bar  */}
-      <div className="flex items-center border  justify-between p-3">
-        <div className="flex items-center justify-center gap-1">
+      <div className="flex items-center mb-3 justify-between p-3">
+        <Link href="/" className="flex items-center justify-center gap-1">
           <Image src="/nav-logo.png" alt="logo" width={20} height={20} />
-          <h2>FreshGo</h2>
-        </div>
+          <h2 className="text-lg text-[#4EA674] font-semibold ">FreshGo</h2>
+        </Link>
         <div>
-          <GoSidebarExpand size={20} />
+          <GoSidebarExpand className="text-green-600" size={20} />
         </div>
       </div>
       {/* links */}
@@ -75,8 +75,8 @@ const Sidebar = () => {
         <div>
           {sidebarLinks.map((side) => {
             return (
-              <div className="border flex flex-col gap-3" key={side.category}>
-                <h1 className="text-sm text-gray-800 border-b">{`${side.category}`}</h1>
+              <div className=" flex flex-col gap-0.5 mt-1 " key={side.category}>
+                <h1 className="text-sm text-start px-3    py-2 text-gray-800 ">{`${side.category}`}</h1>
                 <div>
                   {side.items.map((links) => {
                     return (
@@ -85,8 +85,8 @@ const Sidebar = () => {
                         className=""
                         key={links.name}
                       >
-                        <div className="flex items-center justify-start gap-1.5 border hover:bg-[#4EA674] hover:text-[#FFFFFF] p-2 bg-[#FFFFFF]">
-                          <div>{links.icon}</div>
+                        <div className="flex  items-center justify-start gap-2  hover:bg-[#4EA674] hover:text-[#FFFFFF] p-2.5 rounded-sm bg-[#FFFFFF]">
+                          <div >{links.icon}</div>
                           <h2>{links.name}</h2>
                         </div>
                       </Link>
