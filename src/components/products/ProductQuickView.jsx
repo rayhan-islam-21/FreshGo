0ca62914e4162a76bda16e273955104e60adba0b
Rayhan Islam <merayhanislam21@gmail.com>
2026-02-20 00:11:45 +0600

    final; push brfore sleep

--- src/components/products/ProductQuickView.jsx
+++ src/components/products/ProductQuickView.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { ShoppingCart, Star } from "lucide-react";
+import { Heart, ShoppingCart, Star } from "lucide-react";
 import {
   DialogContent,
   DialogHeader,
@@ -10,10 +10,21 @@ import {
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Separator } from "../ui/separator";
+import { Input } from "../ui/input";
+import { useState } from "react";
 
 const ProductQuickView = ({ product }) => {
   const { price, category, finalprice, name, discount, stock, description } =
     product;
+
+    const [count,setCount] = useState(0);
+
+    const handleIncrement = ()=>{
+        setCount(count+1)
+    }
+    const handleDecrement = ()=>{
+        setCount(count-1)
+    }
 
   return (
     <DialogContent className="min-w-3xl bg-white rounded-xl shadow-2xl border-0">
@@ -49,11 +60,39 @@ const ProductQuickView = ({ product }) => {
           </div>
           <Separator className="bg-gray-500 opacity-10" />
           <div>
-            <p className="text-gray-500">{description.slice(0, 100)}
+            <p className="text-gray-500">
+              {description.slice(0, 100)}
               {description.length > 100 ? "..." : ""}
             </p>
           </div>
           <Separator className="bg-gray-500 opacity-10" />
+          <div className=" flex items-center justify-between">
+            <div className=" relative">
+              <Button onClick ={handleDecrement} className="absolute bg-gray-200 w-6 h-6 place-items-center flex p-2 rounded-full top-1.5 left-2">
+                -
+              </Button>
+              <Input
+                type="number"
+                value={count}
+                min={1}
+                className="border border-black/10  text-center p-4 bg-white  rounded-full w-28 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-moz-appearance]:textfield"
+              />
+
+              <Button
+              onClick={handleIncrement}
+                size="icon"
+                className="absolute bg-gray-200 w-6 h-6 place-items-center flex p-2 rounded-full top-1.5 right-2"
+              >
+                +
+              </Button>
+            </div>
+            <div className="flex-1 flex justify-evenly">
+              <Button variant="outline" className="bg-green-500 text-white">Add to cart</Button>
+              <Button>
+                <Heart size={16} />
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
     </DialogContent>
