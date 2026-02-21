6ce808d1e37340a5f5e1c3d0b921329c3feea61c
Rayhan Islam <merayhanislam21@gmail.com>
2026-02-21 14:53:11 +0600

    cart add cart remove done

--- src/components/cart/SideCart.jsx
+++ src/components/cart/SideCart.jsx
@@ -18,17 +18,16 @@ import { ImCross } from "react-icons/im";
 import { Separator } from "../ui/separator";
 import { IoBagCheckOutline, IoCartOutline } from "react-icons/io5";
 
-
 const SideCart = () => {
   const dispatch = useDispatch();
   const { isOpen, items } = useSelector((state) => state.cart);
-  const handleDelete = ()=>{
-    dispatch((value)=>removeFromCart(value))
-  }
   return (
     <div>
       <Sheet open={isOpen} onOpenChange={(value) => dispatch(setOpen(value))}>
-        <SheetContent side="right" className="bg-white flex min-w-sm p-5 mx-auto">
+        <SheetContent
+          side="right"
+          className="bg-white flex min-w-sm p-5 mx-auto"
+        >
           <SheetHeader>
             <SheetTitle className="">
               Cart Items( {`${items.length}`})
@@ -37,7 +36,10 @@ const SideCart = () => {
           <div className="flex-1">
             {items.map((pro) => {
               return (
-                <div key={pro.id} className="flex gap-4 border-b border-b-gray-300/50 space-y-4 items-center justify-between">
+                <div
+                  key={pro.id}
+                  className="flex gap-4 border-b border-b-gray-300/50 space-y-4 items-center justify-between"
+                >
                   <div className="   place-items-center">
                     <Image
                       src={pro.image}
@@ -49,26 +51,40 @@ const SideCart = () => {
                   </div>
                   <div className="flex-1">
                     <h1 className="text-sm">{pro.name}</h1>
-                    <p className="text-sm text-gray-400">{pro.quantity} {" "}kg x <span className="font-bold text-black">{pro.finalprice}</span> </p>
+                    <p className="text-sm text-gray-400">
+                      {pro.quantity} kg x{" "}
+                      <span className="font-bold text-black">
+                        {pro.finalprice}
+                      </span>{" "}
+                    </p>
                   </div>
-                  <Button size="xs" onClick ={handleDelete} className="p-2 hover:border-black/20 hover:border rounded">
+                  <Button
+                    size="xs"
+                    onClick={() => dispatch(removeFromCart(pro.id))}
+                    className="p-2 hover:border-black/20 hover:border rounded"
+                  >
                     <ImCross className=" text-red-400" size={16} />
                   </Button>
-
                 </div>
-  
               );
             })}
-
           </div>
           <div className="flex flex-col w-full gap-2.5 ">
-            <Button size="lg" variant="outline" className="bg-green-500 cursor-pointer  rounded-sm shadow p-2 text-white">
+            <Button
+              size="lg"
+              variant="outline"
+              className="bg-green-500 cursor-pointer  rounded-sm shadow p-2 text-white"
+            >
               <IoBagCheckOutline size={16} />
-                <span className="text-sm">Checkout</span>
+              <span className="text-sm">Checkout</span>
             </Button>
-            <Button size="lg" variant="outline" className="bg-white rounded-sm hover:bg-green-400 hover:text-white cursor-pointer shadow-sm text-green-500 border-0">
-               <IoCartOutline />
-               <span className="text-sm ">Go To Cart</span>
+            <Button
+              size="lg"
+              variant="outline"
+              className="bg-white rounded-sm hover:bg-green-400 hover:text-white cursor-pointer shadow-sm text-green-500 border-0"
+            >
+              <IoCartOutline />
+              <span className="text-sm ">Go To Cart</span>
             </Button>
           </div>
         </SheetContent>
